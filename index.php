97e37a76abe7ecd8ff5fc5e4d0963389cad64ca3
Kai Burgdorf <k@iburgdorf.de>
2022-04-13 22:56:37 +0200

    something

--- index.php
+++ index.php
@@ -6,27 +6,35 @@ header('Content-Type: text/html; charset=utf-8');
 
 require_once __DIR__ . "/lib/SleekDB-master/src/Store.php";
 
-#valid credentials
-$users = array(
-    "testuser" => "pw",
-    "demouser" => "passwd"
-    );
-
+$databaseDirectory = dirname(__FILE__) . "/db";
 
 $method = (isset($_GET['method'])) ? $_GET['method'] : 'register';
 $username = (isset($_GET['username'])) ? $_GET['username'] : '';
 $password = (isset($_GET['password'])) ? $_GET['password'] : '';
 $email = (isset($_GET['email'])) ? $_GET['email'] : '';
-$apache_headers = getallheaders() || [];
+
+
+
 
 
 if(!strcmp($method, "login")) {
-    if(!validate_credentials($username, $password, $users)) {
+    if(!validate_credentials($username, $password)) {
         echo "login failed";
         exit(0);
     }
+
+	$newStore = new \SleekDB\Store("user", $databaseDirectory, ["timeout" => false]);
+
+	$db_user = $newStore->findOneBy(["username", "=", $username]);
+
     $headers = array('alg'=>'HS256','typ'=>'JWT');
-    $payload = array('user'=>'420','name'=>$username, 'admin'=>true, 'exp'=>(time() + 60));
+    $payload = array(	
+						'sub'	=> $db_user['_id'],
+						'name'	=> $username,
+						'admin'	=> true,
+						'iat'	=> time(), 
+						'exp'	=> (time() + 300)
+					);
 
     $jwt = generate_jwt($headers, $payload);
 
@@ -35,14 +43,13 @@ if(!strcmp($method, "login")) {
 
 
 
-
-
-
-
 if(!strcmp($method, "register")) {
-	$databaseDirectory = "./db";
 	$newStore = new \SleekDB\Store("user", $databaseDirectory, ["timeout" => false, 'primary_key' => "_id"]);
 
+	if($newStore->count(["username", "=", $username]) >  0) {
+		echo "username already set";
+		exit(0);
+	}
 	$user = [
 		"username" => $username,
 		"email" => $email,
@@ -55,10 +62,12 @@ if(!strcmp($method, "register")) {
 }
 
 
+
 if(!strcmp($method, "getServerTime")) {
-	global $apache_headers;
-	if(!is_jwt_valid($apache_headers['Authorization'])) {
-		echo "please login, use token: " . $apache_headers['Authorization'];
+	$apache_headers = getallheaders();
+	$auth_header = $apache_headers['Authorization'];
+	if(!is_jwt_valid($auth_header)) {
+		echo "please login, use token: " . $auth_header;
 		exit(0);
 	}
 
@@ -67,9 +76,7 @@ if(!strcmp($method, "getServerTime")) {
 
 
 
-
 if(!strcmp($method, "getAllUsers")) {
-	$databaseDirectory = "./db";
 	$newStore = new \SleekDB\Store("user", $databaseDirectory, ["timeout" => false]);
 
 	echo json_encode($newStore->findBy(["username", "=", $username]));
@@ -79,9 +86,8 @@ if(!strcmp($method, "getAllUsers")) {
 
 
 
-
-function validate_credentials($username, $password, $users) {
-	$databaseDirectory = "./db";
+function validate_credentials($username, $password) {
+	global $databaseDirectory;
 	$newStore = new \SleekDB\Store("user", $databaseDirectory, ["timeout" => false]);
 
 	$db_user = $newStore->findOneBy(["username", "=", $username]);
@@ -93,9 +99,17 @@ function validate_credentials($username, $password, $users) {
     return false;
 }
 
+
+
+
+
 function base64url_encode($str) {
     return rtrim(strtr(base64_encode($str), '+/', '-_'), '=');
 }
+
+
+
+
 
 function generate_jwt($headers, $payload, $secret = 'secret') {
 	$headers_encoded = base64url_encode(json_encode($headers));
@@ -110,6 +124,10 @@ function generate_jwt($headers, $payload, $secret = 'secret') {
 	return json_encode(array("method" => "login", "jwt" => $jwt));
 }
 
+
+
+
+
 function is_jwt_valid($jwt, $secret = 'secret') {
 	// split the jwt
 	$tokenParts = explode('.', $jwt);
@@ -117,6 +135,7 @@ function is_jwt_valid($jwt, $secret = 'secret') {
 	$payload = base64_decode($tokenParts[1]);
 	$signature_provided = $tokenParts[2];
 
+	//@TODO fix, alway causes error
 	// check the expiration time - note this will cause an error if there is no 'exp' claim in the jwt
 	$expiration = json_decode($payload)->exp;
 	$is_token_expired = ($expiration - time()) < 0;
@@ -136,8 +155,5 @@ function is_jwt_valid($jwt, $secret = 'secret') {
 		return TRUE;
 	}
 }
-#if method is signup
-#
-
 
 ?>
